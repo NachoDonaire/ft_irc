5a44d4cd35e2389feb553cfe4f7ed1c477b5dd8a
Daniel Exposito lopez <dexposit@c1r9s5.42madrid.com>
2024-05-09 20:41:45 +0200

    define getter and setter as well as main as the prototypes of the main features of the Channel class

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -2,31 +2,54 @@
 #define CHANNEL_H
 #include <iostream>
 #include <string>
+typedef std::vector<std::string> 	vectorStr;
+typedef std::string 			str;
 
 class Channel
 {
 public:
 	Channel();
 	~Channel();
-   Channel(const Channel& src);
-   Channel& operator=(const Channel& src);
+   	Channel(const Channel& src);
+   	Channel& 	operator=(const Channel& src);
 	//joinClient
+	void joinClient(const str& userId, const bool& isAdmin);
 	//deleteClient
+	void deleteClient(const str& userId);
 	//kick
 	//invite
+	void invite(const str& senderId, const str& receiverId);
 	//changeTopic
+	void changeTopic(const str& userId, const str& value);
 	//changeInviteMode
+	void changeInviteMode(const str& userId, const bool& value);
 	//changePassword
+	bool 	findUser(const str& user);
+
+	vectorStr	getUsers() const;
+	vectorStr	getAdmins() const;
+	str		getTopic() const;
+	str		getId() const;
+	str		getPassword() const;
+	unsigned int 	getMaxUsers() const;
+	bool 		getInviteMode() const;
+
+	void		setUsers(vectorStr src);
+	void		setAdmins(vectorStr src);
+	void		setTopic(str src);
+	void		setId(str src);
+	void		setPassword(str src);
+	void		setMaxUsers(unsigned int src);
+	void		setInviteMode(bool src);
+	
 private:
-	//vector de usuarios del canal
-	//vector de admin users
-	//std::string topic
-	//std::string idName
-	//std::string password
-	//vector de idUsers blacklist
-	//vector de idUsers whitelist
-	//bool invite
-	//int maxUsers;
+	vectorStr	users;
+	vectorStr 	admins;
+	std::string 	topic;
+	std::string 	id;
+	std::string 	password;
+	unsigned int 	maxUsers;
+	bool		inviteMode;
 };
 
 #endif     //CHANNEL_H
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,6 +1,7 @@
+#include <Ircsrv.hpp>
 #include <Channel.hpp>
 
-Channel::Channel()
+Channel::Channel(): users(0), admins(0), topic(""), id(""), password(""), maxUsers(0), inviteMode(false)
 {
 //Const
 }
@@ -18,5 +19,78 @@ Channel::~Channel()
 
 Channel& Channel::operator = (const Channel& src)
 {
+	users = src.getUsers();
 	return *this;
 }
+
+vectorStr Channel::getUsers(void) const
+{
+	return this->users;
+}
+
+vectorStr Channel::getAdmins() const
+{
+	return this->admins;
+}
+
+str	Channel::getTopic() const
+{
+	return this->topic;
+}
+
+str	Channel::getId() const
+{
+	return this->id;
+}
+
+str	Channel::getPassword() const
+{
+	return this->password;
+}
+
+unsigned int Channel::getMaxUsers() const
+{
+	return this->maxUsers;
+}
+
+bool	Channel::getInviteMode() const
+{
+	return this->inviteMode;
+}
+
+void 	Channel::setUsers(vectorStr src)
+{
+	this->users = src;
+}
+
+void	Channel::setAdmins(vectorStr src)
+{
+	this->admins = src;
+}
+
+void	Channel::setTopic(str src)
+{
+	this->topic = src;
+}
+
+void	Channel::setId(str src)
+{
+	this->id = src;
+}
+
+void	Channel::setPassword(str src)
+{
+	this->password = src;
+}
+
+void	Channel::setMaxUsers(unsigned int src)
+{
+	this->maxUsers = src;
+}
+
+void	Channel::setInviteMode(bool src)
+{
+	this->inviteMode = src;
+}
+
+
