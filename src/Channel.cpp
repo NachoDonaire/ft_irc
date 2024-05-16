ceae28b75219caa73766df74c54d1a7196f6bd21
Daniel Exposito lopez <dexposit@c1r17s4.42madrid.com>
2024-05-16 20:38:34 +0200

    correct joinClient() and add new method isRegister(userid)

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -103,27 +103,28 @@ void	Channel::setTopicRestriction(const bool& src)
 	this->topicRestriction = src;
 }
 
-void	Channel::joinClient(const str& userId, const bool& isAdmin)
+void	Channel::joinClient(const str& userId, const str& password, const bool& isAdmin)
 {
-	try
-	{
+	std::cout << "llega hasta aqui y mete al usuario" << std::endl;
+	if (password != this->password)
+		throw std::logic_error("The password provided is not valid for the channel");
+	if (!isAdmin)
 		joinClient(users, userId, isAdmin);
-	}
-	catch (std::exception& e)
-	{
+	else
 		joinClient(admins, userId, isAdmin);
-	}
 }
 void 	Channel::joinClient(vectorStr& users, const str& userId, const bool& isAdmin)
 {
 	if (userId == "")
 		throw std::logic_error("Provide a valid userId to join to the Channel.");
-	if (users.size() == maxUsers)
+	if (users.size() + admins.size() >= maxUsers && maxUsers >= 0)
 		throw std::logic_error("The Channel is full");
 	vectorStr::iterator user = findUser(users, userId);
-	if (user != users.end())
+	if (user != users.end() || findUser(admins, userId) != admins.end())
 		throw std::logic_error("The user is already in the Channel");
-	users.push_back(userId);
+
+	isAdmin ? admins.push_back(userId) : users.push_back(userId);
+	std::cout << "llega hasta aqui y mete al usuario" << std::endl;
 }
 
 void	Channel::deleteClient(vectorStr& users, const str& userId)
@@ -193,3 +194,10 @@ void	Channel::changePassword(const str& userId, const str& newPassword)
 	this->password = newPassword;
 }
 
+bool	Channel::isRegister(str& userId)
+{
+	vectorStr::iterator user = findUser(users, userId);
+	if (user == users.end())
+		user = findUser(admins, userId);
+	return (user != admins.end());
+}
