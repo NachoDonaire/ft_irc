4d0e42cb8fc3ae4e90ae4d1de430cda44ebde9b5
NachoDonaire <nacho23donaire@gmail.com>
2024-08-04 01:17:47 +0200

    adding port as arg to leaks.sh

--- test_scripts/leaks.sh
+++ test_scripts/leaks.sh
@@ -1,5 +1,6 @@
+port=$1
 valgrind --leak-check=full \
          --show-leak-kinds=all \
          --track-origins=yes \
          --verbose \
-         ./ircserv 6668 kaka
+         ./ircserv $port kaka
